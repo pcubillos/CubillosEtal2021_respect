06af6c11e4f8519ee1afffe61d4861e8e7f1edc3
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2020-10-01 14:26:53 +0200

    Updated compendium.

--- compendium.sh
+++ compendium.sh
@@ -2,17 +2,18 @@
 topdir=`pwd`
 
 # Installs:
-pip install mc3==3.0.0
-pip install pyratbay==0.9.0a5
-#pip install pandexo.engine==1.4
-#pip install scipy==1.3.3
+pip install mc3==3.0.2
+pip install pyratbay==0.9.1
+pip install scipy==1.3.3
+
 cd $topdir
 git clone https://github.com/pcubillos/rate
 
 git clone --recursive https://github.com/natashabatalha/pandexo
 cd pandexo
-# either git checkout b9f1f06+pandeia 1.5 or edit
+git checkout b9f1f06
 python setup.py install
+# You will need all the pandeia 1.5 business (see pandexo docs)
 
 # Generate filter files:
 cd $topdir/code
@@ -26,7 +27,7 @@ wget http://kurucz.harvard.edu/grids/gridp00aodfnew/fp00ak2odfnew.pck
 # Download repack-exomol/hitemp data:
 cd $topdir/inputs/opacity
 wget -i wget_repack-exomol_H2O-CH4.txt
-wget -i wget_hitemp_CO_CO2.txt
+wget -i wget_hitemp_CO-CO2.txt
 unzip '*.zip'
 rm -f *.zip
 bzip2 -d 05_HITEMP2019.par.bz2
@@ -44,11 +45,9 @@ pbay -c tli_CO2_hitemp_2010.cfg
 pbay -c tli_H2O_exomol_pokazatel.cfg
 pbay -c tli_CH4_exomol_yt10to10.cfg
 
-
 # Make atmospheric files:
 cd $topdir/run_setup
 pbay -c atm_equilibrium.cfg
-pbay -c atm_vp_gcm.cfg
 
 # Make opacity files:
 cd $topdir/run_setup
@@ -57,23 +56,21 @@ pbay -c opacity_CH4_0.8-5.5um_R10000.cfg
 pbay -c opacity_CO2_0.8-5.5um_R10000.cfg
 pbay -c opacity_CO_0.8-5.5um_R10000.cfg
 
-pbay -c opacity_H2O_0.6-12.0um_R10000.cfg
-pbay -c opacity_CH4_0.6-12.0um_R10000.cfg
-pbay -c opacity_CO2_0.6-12.0um_R10000.cfg
-pbay -c opacity_CO_0.6-12.0um_R10000.cfg
 
-# Run retrieval:
-cd $topdir/run_resolved/
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Run HST+Spitzer retrieval:
+cd $topdir/run_resolved
 pbay -c mcmc_WASP43b_day_resolved.cfg
 pbay -c mcmc_WASP43b_east_resolved.cfg
 pbay -c mcmc_WASP43b_west_resolved.cfg
 
-cd $topdir/run_integrated/
+cd $topdir/run_integrated
 pbay -c mcmc_WASP43b_day_integrated.cfg
 pbay -c mcmc_WASP43b_east_integrated.cfg
 pbay -c mcmc_WASP43b_west_integrated.cfg
 
 
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # Model WASP-43b phase curve:
 cd $topdir/run_simulation
 python ../code/model_WASP43b.py
@@ -81,20 +78,9 @@ python ../code/make_filters.py
 python ../code/fig_WASP43b_model_atmosphere.py
 python ../code/fig_WASP43b_model_spectra.py
 
-# Retrieve model WASP-43b spectra:
-cd $topdir/run_resolved_sim
-pbay -c mcmc_model_WASP43b_hot_resolved.cfg
-pbay -c mcmc_model_WASP43b_day_resolved.cfg
-pbay -c mcmc_model_WASP43b_east_resolved.cfg
-pbay -c mcmc_model_WASP43b_west_resolved.cfg
-pbay -c mcmc_model_WASP43b_night_resolved.cfg
-
-cd $topdir/run_integrated_sim
-pbay -c mcmc_model_WASP43b_hot_integrated.cfg
-pbay -c mcmc_model_WASP43b_day_integrated.cfg
-pbay -c mcmc_model_WASP43b_east_integrated.cfg
-pbay -c mcmc_model_WASP43b_west_integrated.cfg
-pbay -c mcmc_model_WASP43b_night_integrated.cfg
-
+# Retrieve simulated JWST WASP-43b spectra:
+cd $topdir
+sh inputs/launch_jwst_sim_resolved.sh
+sh inputs/launch_jwst_sim_integrated.sh
 
 
---
+++ inputs/launch_jwst_sim_resolved.sh
@@ -0,0 +1,18 @@
+cd run_jwst
+
+pbay -c mcmc_model_WASP43b_resolved_phase0.00.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.06.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.12.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.19.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.25.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.31.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.38.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.44.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.50.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.56.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.62.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.69.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.75.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.81.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.88.cfg
+pbay -c mcmc_model_WASP43b_resolved_phase0.94.cfg
