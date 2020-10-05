ce6180568c12d8422daf018f76c7ad2dc72d0212
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2020-10-05 11:19:15 +0200

    Updated data and forward model figures.

---
+++ code/fig_WASP43b_data_retrieval.py
@@ -0,0 +1,208 @@
+import os
+
+import matplotlib
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d as gaussf
+import matplotlib.pyplot as plt
+plt.ion()
+
+import pyratbay as pb
+import pyratbay.atmosphere as pa
+import pyratbay.constants as pc
+import pyratbay.tools as pt
+
+import mc3
+import mc3.plots as mp
+
+
+retrievals = [
+    'run_resolved/mcmc_WASP43b_east_resolved.cfg',
+    'run_resolved/mcmc_WASP43b_day_resolved.cfg',
+    'run_resolved/mcmc_WASP43b_west_resolved.cfg',
+    'run_integrated/mcmc_WASP43b_east_integrated.cfg',
+    'run_integrated/mcmc_WASP43b_day_integrated.cfg',
+    'run_integrated/mcmc_WASP43b_west_integrated.cfg',
+    ]
+
+
+bestp, best_spec, data, uncert = [], [], [], []
+bestPT, pt_low, pt_high, pt_median = [], [], [], []
+posterior = []
+for cfg in retrievals:
+    with pt.cd(os.path.dirname(cfg)):
+        pyrat = pb.run(os.path.basename(cfg), init=True, no_logfile=True)
+
+    with np.load(pyrat.ret.mcmcfile) as mcmc:
+        post, zchain, zmask = mc3.utils.burn(mcmc)
+        bestp.append(mcmc["bestp"])
+        best_spec.append(pyrat.eval(mcmc["bestp"])[0])
+
+    data.append(pyrat.obs.data)
+    uncert.append(pyrat.obs.uncert)
+    posterior.append(post)
+    # Posterior PT profiles:
+    ifree = pyrat.ret.pstep[pyrat.ret.itemp] > 0
+    itemp = np.arange(np.sum(ifree))
+    tpost = pa.temperature_posterior(
+        post[:,itemp], pyrat.atm.tmodel, pyrat.ret.params[pyrat.ret.itemp],
+        ifree, pyrat.atm.press)
+    pt_median.append(tpost[0])
+    pt_low.append(tpost[1])
+    pt_high.append(tpost[2])
+
+
+medians = [np.median(post, axis=0) for post in posterior]
+plows  = [np.percentile(post, 15.865, axis=0) for post in posterior]
+phighs = [np.percentile(post, 84.135, axis=0) for post in posterior]
+
+for median, plow, phigh in zip(medians, plows, phighs):
+    print("")
+    for med,lo,hi in zip(median, plow, phigh):
+        low = med-lo
+        high = hi-med
+        print(f'${med:.1f}_{{-{low:.1f}}}^{{+{high:.1f}}}$')
+
+
+
+swest_posteriors = posterior[0][:,3:7] #[:,4:8]
+sday_posteriors  = posterior[1][:,3:7] #[:,4:8]
+seast_posteriors = posterior[2][:,3:7] #[:,4:8]
+
+dwest_posteriors = posterior[3][:,3:7] #[:,4:8]
+dday_posteriors  = posterior[4][:,3:7] #[:,4:8]
+deast_posteriors = posterior[5][:,3:7] #[:,4:8]
+
+
+bandwl = 1e4/pyrat.obs.bandwn
+pressure = pyrat.atm.press/pc.bar
+sflux = pyrat.spec.starflux
+rprs = pyrat.phy.rplanet/pyrat.phy.rstar
+wl = 1.0/(pyrat.spec.wn*pc.um)
+
+labels = [
+    "Phase = 0.25 (east)",
+    "Phase = 0.50 (dayside)",
+    "Phase = 0.75 (west)",
+    ]
+
+dcolors = ["mediumblue", "forestgreen", "red"]
+scolors = ["cornflowerblue", "limegreen", "darkorange"]
+
+themes = [
+    {'edgecolor': 'mediumblue',
+     'facecolor': 'mediumblue',
+     'color':     'mediumblue', },
+    {'edgecolor': 'cornflowerblue',
+     'facecolor': 'cornflowerblue',
+     'color':     'cornflowerblue', },
+    {'edgecolor': 'forestgreen',
+     'facecolor': 'forestgreen',
+     'color':     'forestgreen', },
+    {'edgecolor': 'limegreen',
+     'facecolor': 'limegreen',
+     'color':     'limegreen', },
+]
+
+pnames = [
+    '$\\log_{10}(X_{\\rm H2O})$',
+    '$\\log_{10}(X_{\\rm CO})$',
+    '$\\log_{10}(X_{\\rm CO2})$',
+    '$\\log_{10}(X_{\\rm CH4})$',
+    ]
+
+
+fs = 10
+sigma = 15.0
+lw = 1.0
+f0 = 1.0 / pc.ppt
+
+temp_lims = 400, 2400
+xt, yt = 0.7, 0.05
+dxt = 1.0 - xt - 0.02
+dyt = 0.5 - yt - 0.04
+
+xs, ys = 0.07, 0.03
+dxs = 0.62 - xs 
+dys = 1.0/3 - ys - 0.03
+
+rect = 0.03, 0.05, 0.62, 0.31
+margin = 0.01
+ranges = [(-6, -1), (-12, -1), (-12, -1), (-12, -1)]
+
+plt.figure(10, (8.5, 9))
+plt.clf()
+ax = plt.axes([xt, yt+0.5, dxt, dyt])
+for tm, tl, th, col, lab in \
+        zip(pt_median[3:], pt_low[3:], pt_high[3:], dcolors, labels):
+    plt.plot(tm, pressure, c=col, label=lab)
+    ax.fill_betweenx(pressure, tl, th, facecolor=col, alpha=0.25)
+ax.tick_params(labelsize=fs, direction='in')
+ax.set_yscale("log")
+ax.set_ylim(np.amax(pressure), np.amin(pressure))
+ax.set_ylim(100, 1e-6)
+ax.set_xlim(temp_lims)
+ax.set_xlabel("Temperature (K)", fontsize=fs)
+ax.set_ylabel("Pressure (bar)", fontsize=fs)
+ax.legend(loc='upper right', fontsize=fs-1)
+ax.set_title('Disk integrated', fontsize=fs)
+
+ax = plt.axes([xt, yt, dxt, dyt])
+for tm, tl, th, col, lab in \
+        zip(pt_median, pt_low, pt_high, scolors, labels):
+    plt.plot(tm, pressure, c=col, label=lab)
+    ax.fill_betweenx(pressure, tl, th, facecolor=col, alpha=0.25)
+ax.tick_params(labelsize=fs, direction='in')
+ax.set_yscale("log")
+ax.set_ylim(np.amax(pressure), np.amin(pressure))
+ax.set_ylim(100, 1e-6)
+ax.set_xlim(temp_lims)
+ax.set_xlabel("Temperature (K)", fontsize=fs)
+ax.set_ylabel("Pressure (bar)", fontsize=fs)
+ax.legend(loc='upper right', fontsize=fs-1)
+ax.set_title('Longitudinally resolved', fontsize=fs)
+
+# Spectra:
+for bflux, datum, err, col in \
+      zip(best_spec, data, uncert, scolors+dcolors):
+    offset = 1.002 if col not in dcolors else 1.0
+    ax = plt.axes([xs, ys+2.0/3, dxs, dys])
+    ax.plot(wl, f0*gaussf(bflux/sflux*rprs**2, sigma), c=col, lw=lw)
+    ax.errorbar(bandwl*offset, f0*datum, f0*err, fmt="o", c=col, zorder=100,
+                 ms=4.0, mew=0.5, mec="k")
+    ax.set_xscale('log')
+    ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    plt.gca().xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+    ax.set_xticks([1.1, 1.4, 1.7, 2, 2.4, 3, 4, 5])
+    ax.set_xlim(1.05, 5.2)
+    ax.set_ylim(0.0, 5)
+    ax.set_xlabel("Wavelength (um)", fontsize=fs)
+    ax.set_ylabel(r"$F_{\rm p}/F_{\rm s}$ (ppt)", fontsize=fs)
+    # WFC3
+    ax = plt.axes([xs, ys+1.0/3, dxs, dys])
+    ax.plot(wl, f0*gaussf(bflux/sflux*rprs**2, sigma), c=col, lw=lw)
+    ax.errorbar(bandwl*offset, f0*datum, f0*err, fmt="o", c=col, zorder=100,
+                 ms=4.0, mew=0.5, mec="k")
+    ax.set_xlim(1.1, 1.7)
+    ax.set_ylim(0.0, 0.9)
+    ax.set_xlabel("Wavelength (um)", fontsize=fs)
+    ax.set_ylabel(r"$F_{\rm p}/F_{\rm s}$ (ppt)", fontsize=fs)
+
+# Volume mixing ratios
+axes = [mc3.plots.subplotter(rect, margin, i, nx=4, ny=3, ymargin=0.0)
+    for i in range(1,13)]
+margin = 0.01
+mp.histogram(dwest_posteriors, ranges=ranges, axes=axes[0: 4], theme=themes[0])
+mp.histogram(swest_posteriors, ranges=ranges, axes=axes[0: 4], theme=themes[1])
+mp.histogram(dday_posteriors,  ranges=ranges, axes=axes[4: 8], theme=themes[2])
+mp.histogram(sday_posteriors,  ranges=ranges, axes=axes[4: 8], theme=themes[3])
+mp.histogram(deast_posteriors, ranges=ranges, axes=axes[8:12], theme='red')
+mp.histogram(seast_posteriors, ranges=ranges, axes=axes[8:12], theme='orange',
+    pnames=pnames, fs=fs)
+for i in range(8):
+    axes[i].set_xticklabels([])
+for i in range(8,12):
+    plt.setp(axes[i].xaxis.get_majorticklabels(), rotation=0)
+
+plt.savefig("plots/WASP43b_retrieval.pdf")
+
+
--- code/fig_WASP43b_model_spectra.py
+++ code/fig_WASP43b_model_spectra.py
@@ -1,9 +1,48 @@
+import sys
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter1d as gaussf
-
 import pyratbay.constants as pc
+
+sys.path.append('../rate')
+import rate
+
+# Make 3D sphere
+nlon = 48
+nlat = 24
+n = nlon//4
+
+u = np.linspace(0, 2 * np.pi, nlon)
+v = np.linspace(0, np.pi, nlat)
+x = 10 * np.outer(np.cos(u), np.sin(v))
+y = 10 * np.outer(np.sin(u), np.sin(v))
+z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
+
+u_cell = u[34:36]
+v_cell = v[8:10]
+r_cell = 10.1
+x_cell = r_cell * np.outer(np.cos(u_cell), np.sin(v_cell))
+y_cell = r_cell * np.outer(np.sin(u_cell), np.sin(v_cell))
+z_cell = r_cell * np.outer(np.ones(np.size(u_cell)), np.cos(v_cell))
+
+r = 10.1
+u_slice = u[32:39:4]
+v_slice = v[0:19]
+x_slice = r * np.outer(np.cos(u_slice), np.sin(v_slice))
+y_slice = r * np.outer(np.sin(u_slice), np.sin(v_slice))
+z_slice = r * np.outer(np.ones(np.size(u_slice)), np.cos(v_slice))
+
+yy = np.linspace(-0.25*np.pi, 0.5*np.pi, 100)
+xx = -np.cos(yy)/10.0
+
+colors = np.zeros(z.shape + (4,))
+for i in range(n):
+    colors[4*i:4*(i+1)] = plt.cm.viridis(np.sin(np.pi*i/(n-1)))
+wcolors = np.copy(colors)
+wcolors[:,:,0:3] = 0.0
+azim, elev = 22.5, 270.0
 
 
 # Bicolor map:
@@ -18,7 +57,6 @@ bicolor = matplotlib.colors.ListedColormap(newcolors)
 with np.load('WASP43b_3D_synthetic_emission_spectra.npz') as emission_model:
     flux = emission_model['spectra']
     wl = emission_model['wavelength']
-    obs_phase = emission_model['phase']
     starflux = emission_model['starflux']
     rprs = emission_model['rprs']
 
@@ -26,65 +64,205 @@ with np.load('WASP43b_3D_synthetic_pandexo_flux_ratios.npz') as sim:
     pwl = sim['pandexo_wl']
     pflux = sim['pandexo_flux_ratio']
     puncert = sim['pandexo_uncert']
+    obs_phase = sim['phase']
+
+with np.load('WASP43b_3D_temperature_madhu_model.npz') as gcm_data:
+    temps = gcm_data['temp']
+    press = gcm_data['press']/pc.bar
+    phase = gcm_data['phase']
+    obs_ilat = gcm_data['obs_ilat']
+
+
+# Composition:
+r = rate.Rate(C=2.5e-4, N=1.0e-4, O=5.0e-4, fHe=0.0851)
+ipress = 42  # ~0.1 bar
+ilat = 0  # -60 deg  (Nice switch from CO--CH4 dominated atm)
+p = np.tile(press[ipress], nphase)  # bars
+rtemp = temps[:,ilat,ipress]     # kelvin
+Q = r.solve(rtemp, p)
+rate_spec = np.array([0, 1, 2, 3])
+rc = 'navy orange limegreen r deepskyblue'.split()
+labels = r.species
+
+ilon = np.concatenate([[nphase-1], np.arange(nphase), [0]])
+phase = obs_phase[ilon]
+phase[0] -= 1
+phase[-1] += 1
+logQ = np.log(Q[:, ilon])
+
+# Abundances at lat = -60 deg
+plt.figure(5)
+plt.clf()
+ax = plt.subplot(111)
+for j,ir in enumerate(rate_spec):
+    ax.plot(phase, logQ[ir], 'o-', c=rc[j], ms=2, label=r.species[ir])
+ax.tick_params(labelsize=fs-1)
+#plt.yscale('log')
+ax.set_xlabel("Orbital Phase", fontsize=fs)
+ax.set_ylabel(r'$\log_{10}$(Volume mixing ratio)', fontsize=fs-1)
+ax.set_xlim(0, 1)
+ax.set_ylim(-25, -5)
+ax.legend(loc='upper right', fontsize=fs-2)
+plt.tight_layout()
+
 
 
 nphase = len(obs_phase)
 
-fs = 13
-bot, dy = 0.11, 0.84
+fs = 11
+bot, dy = 0.10, 0.88
 left1, left2, dx1, dx2 = 0.09, 0.41, 0.25, 0.56
-logxticks = 0.6, 1.0, 1.4, 2.0, 3.0, 5.0, 7.0, 10.0
+logxticks = 0.8, 1.0, 1.4, 2.0, 3.0, 4.0, 5.0
 sigma = 15.0
 
 
-fig = plt.figure(47, (8,5))
+fig = plt.figure(47, (8.5, 4.5))
 plt.clf()
-ax = fig.add_axes([0.11, bot, 0.77, dy])
+
+ax = plt.axes([-0.06, 0.55, 0.35, 0.55], projection='3d')
+ax.axis('off')
+ax.view_init(azim, elev)
+ax.plot_surface(
+    x, y, z, facecolors=colors, linewidth=0.0, antialiased=True, zorder=3)
+ax.plot_wireframe(x_cell, y_cell, z_cell, color='k', linewidth=1.0, zorder=5)
+ax.plot_wireframe(
+    x_slice, y_slice, z_slice, color='k', linewidth=1.0, zorder=5, ccount=0)
+
+ax2 = plt.axes([-0.03, 0.55, 0.35, 0.88])
+ax2.axis('off')
+ax2.plot(xx+0.01, yy, color='0.5')
+ax2.plot(-xx+0.087, 1.1*yy+0.6, color='0.5')
+ax2.set_xlim(-0.22, 0.5)
+ax2.set_ylim(-2.5, 13.0)
+ax2.text(-0.02, -0.75, r"32x64 lat$-$lon grid", fontsize=fs-2)
+ax2.text(-0.09, -1.4, "16 longitudinal slices", fontsize=fs-2)
+
+ax = fig.add_axes([0.068, 0.11, 0.15, 0.44])
+for i in range(nphase):
+    plt.semilogy(temps[i,1], press, lw=1.5, c=bicolor(obs_phase[i]))
+ax.set_yticks(np.logspace(2, -6, 5))
+ax.set_ylim(np.amax(press), 1e-7)
+ax.set_xlim(250, 2200)
+ax.set_ylabel('Pressure (bar)', fontsize=fs-1)
+plt.xlabel('Temperature (K)', fontsize=fs-1)
+ax.tick_params(labelsize=fs-2, direction='in', which='both')
+
+ax = fig.add_axes([0.29, bot, 0.63, dy])
 plt.xscale('log')
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax.set_xticks(logxticks)
 for iphase in range(nphase):
-    plt.plot(wl, gaussf(flux[iphase],sigma), c=bicolor(obs_phase[iphase]))
-plt.xlim(1.0, 6.0)
-plt.xlim(0.6, 12.0)
-plt.ylim(0, 90000)
-plt.xlabel('Wavelength (um)', fontsize=fs)
-plt.ylabel(r'Disk-integrated Flux (erg s$^{-1}$ cm$^{-2}$ cm)', fontsize=fs)
+    ax.plot(wl, gaussf(flux[iphase],sigma)*pc.ppt, c=bicolor(obs_phase[iphase]))
+ax.set_xlim(0.8, 5.5)
+ax.set_ylim(0, 110)
+ax.set_xlabel('Wavelength (um)', fontsize=fs)
+ax.set_ylabel(
+    r'Disk-integrated flux ($10^{3}$ erg s$^{-1}$ cm$^{-2}$ cm)', fontsize=fs)
+ax.tick_params(labelsize=fs-1, direction='in')
 
-ax1 = fig.add_axes([0.9, bot, 0.025, dy])
+ax1 = fig.add_axes([0.925, bot, 0.015, dy])
 norm = matplotlib.colors.Normalize(vmin=0, vmax=1.0)
 cb1 = matplotlib.colorbar.ColorbarBase(
     ax1, cmap=bicolor, norm=norm, ticks=np.linspace(0,1,11),
     orientation='vertical')
-cb1.set_label('Orbital Phase', fontsize=fs)
+cb1.set_label('Orbital phase', fontsize=fs)
+ax1.tick_params(labelsize=fs-1, direction='in')
 cb1.update_ticks()
-plt.savefig('../plots/model_WASP43b_disk_integrated_emission.pdf')
+
+ax = fig.add_axes([0.364, 0.69, 0.22, 0.28])
+for j,ir in enumerate(rate_spec):
+    ax.plot(phase, Q[ir, ilon], 'o-', c=rc[j], ms=2, label=r.species[ir])
+ax.tick_params(labelsize=fs-2, direction='in')
+ax.set_yscale('log')
+ax.set_xlabel("Orbital Phase", fontsize=fs-1)
+ax.set_ylabel(r'Volume mixing ratio   ', fontsize=fs-1)
+ax.set_xticks([0, 0.5, 1.0])
+ax.set_yticks([1e-3, 1e-6, 1e-9])
+ax.set_xlim(0, 1)
+ax.set_ylim(3e-11, 3e-3)
+ax.legend(fontsize=fs-2.5, loc=(1.03,0.46),
+    labelspacing=0.2,
+    handlelength=1.25,
+    handletextpad=0.5,
+    )
+plt.savefig('../plots/WASP43b_disk_integrated_model_emission.pdf')
 
 
-fig = plt.figure(48, (8,5))
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+fig = plt.figure(48, (8.5, 4.5))
 plt.clf()
-ax = fig.add_axes([0.11, bot, 0.77, dy])
+
+ax = plt.axes([-0.06, 0.55, 0.35, 0.55], projection='3d')
+ax.axis('off')
+ax.view_init(azim, elev)
+ax.plot_surface(
+    x, y, z, facecolors=colors, linewidth=0.0, antialiased=True, zorder=3)
+ax.plot_wireframe(x_cell, y_cell, z_cell, color='k', linewidth=1.0, zorder=5)
+ax.plot_wireframe(
+    x_slice, y_slice, z_slice, color='k', linewidth=1.0, zorder=5, ccount=0)
+
+ax2 = plt.axes([-0.03, 0.55, 0.35, 0.88])
+ax2.axis('off')
+ax2.plot(xx+0.01, yy, color='0.5')
+ax2.plot(-xx+0.087, 1.1*yy+0.6, color='0.5')
+ax2.set_xlim(-0.22, 0.5)
+ax2.set_ylim(-2.5, 13.0)
+ax2.text(-0.02, -0.75, r"32x64 lat$-$lon grid", fontsize=fs-2)
+ax2.text(-0.09, -1.4, "16 longitudinal slices", fontsize=fs-2)
+
+ax = fig.add_axes([0.068, 0.11, 0.16, 0.44])
+for i in range(nphase):
+    plt.semilogy(temps[i,1], press, lw=1.5, c=bicolor(obs_phase[i]))
+ax.set_yticks(np.logspace(2, -6, 5))
+ax.set_ylim(np.amax(press), 1e-7)
+ax.set_xlim(250, 2200)
+ax.set_ylabel('Pressure (bar)', fontsize=fs-1)
+plt.xlabel('Temperature (K)', fontsize=fs-1)
+ax.tick_params(labelsize=fs-2, direction='in', which='both')
+
+ax = fig.add_axes([0.29, bot, 0.63, dy])
 plt.xscale('log')
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax.set_xticks(logxticks)
 for iphase in range(nphase):
-    flux_ratio = 1e6*flux[iphase]/starflux * rprs**2
+    flux_ratio = flux[iphase]/starflux * rprs**2 / pc.ppt
     plt.plot(wl, gaussf(flux_ratio,sigma), c=bicolor(obs_phase[iphase]))
-plt.xlim(0.6, 12.0)
-plt.ylim(0, 8000)
-plt.xlabel('Wavelength (um)', fontsize=fs)
-plt.ylabel(r'Disk-integrated $F_{\rm p}/F_{\rm s}$ (ppm)', fontsize=fs)
+ax.set_xlim(0.8, 5.5)
+ax.set_ylim(0, 5.3)
+ax.set_xlabel('Wavelength (um)', fontsize=fs)
+ax.set_ylabel(r'Disk-integrated $F_{\rm p}/F_{\rm s}$ (ppt)', fontsize=fs)
+ax.tick_params(labelsize=fs-1, direction='in')
 
-ax1 = fig.add_axes([0.9, bot, 0.025, dy])
+ax1 = fig.add_axes([0.925, bot, 0.015, dy])
 norm = matplotlib.colors.Normalize(vmin=0, vmax=1.0)
 cb1 = matplotlib.colorbar.ColorbarBase(
     ax1, cmap=bicolor, norm=norm, ticks=np.linspace(0,1,11),
     orientation='vertical')
-cb1.set_label('Orbital Phase', fontsize=fs)
+cb1.set_label('Orbital phase', fontsize=fs)
+ax1.tick_params(labelsize=fs-1, direction='in')
 cb1.update_ticks()
-plt.savefig('../plots/model_WASP43b_disk_integrated_flux_ratio.pdf')
+
+ax = fig.add_axes([0.37, 0.65, 0.25, 0.3])
+for j,ir in enumerate(rate_spec):
+    ax.plot(phase, Q[ir, ilon], 'o-', c=rc[j], ms=2, label=r.species[ir])
+ax.tick_params(labelsize=fs-2, direction='in')
+ax.set_yscale('log')
+ax.set_xlabel("Orbital Phase", fontsize=fs-1)
+ax.set_ylabel(r'Volume mixing ratio', fontsize=fs-1)
+ax.set_xticks([0, 0.5, 1.0])
+ax.set_yticks([1e-3, 1e-6, 1e-9])
+ax.set_xlim(0, 1)
+ax.set_ylim(3e-11, 3e-3)
+ax.legend(fontsize=fs-2.5, loc=(1.03,0.5),
+    labelspacing=0.2,
+    handlelength=1.25,
+    handletextpad=0.5,
+    )
+plt.savefig('../plots/WASP43b_disk_integrated_model_flux_ratio.pdf')
 
 
+
+# Noised data:
 sigma = 10.0
 logxticks = 1.0, 1.4, 2.0, 3.0, 4.0, 5.0
 
