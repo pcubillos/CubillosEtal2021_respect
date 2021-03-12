6a9558d128208b252566cc2f8953cbce82946a58
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2021-03-12 17:24:27 +0100

    Updated script to generate model WASP-43b phase-curve spectra. Added npz file with Madhu-TP fit to WASP-43b GCM model.

--- code/fig_WASP43b_model_spectra.py
+++ code/fig_WASP43b_model_spectra.py
@@ -1,15 +1,11 @@
-import sys
-
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter1d as gaussf
 import pyratbay.constants as pc
 
-sys.path.append('../rate')
-import rate
 
-# Make 3D sphere
+# 3D sphere:
 nlon = 48
 nlat = 24
 n = nlon//4
@@ -63,9 +59,7 @@ with np.load('WASP43b_3D_temperature_madhu_model.npz') as gcm_data:
 with np.load('WASP43b_3D_synthetic_emission_spectra.npz') as emission_model:
     flux = emission_model['spectra']
     wl = emission_model['wavelength']
-    starflux = emission_model['starflux']
-    rprs = emission_model['rprs']
-    obs_phase = emission_model['obs_phase']
+    abundances = emission_model['abundances']
 
 with np.load('WASP43b_3D_synthetic_pandexo_flux_ratios.npz') as sim:
     pwl = sim['pandexo_wl']
@@ -76,22 +70,10 @@ with np.load('WASP43b_3D_synthetic_pandexo_flux_ratios.npz') as sim:
 nphase = len(obs_phase)
 
 # Composition:
-r = rate.Rate(C=2.5e-4, N=1.0e-4, O=5.0e-4, fHe=0.0851)
-ipress = 42  # ~0.1 bar
-ilat = 0  # -60 deg  (Nice switch from CO--CH4 dominated atm)
-p = np.tile(press[ipress], nphase)  # bars
-rtemp = temps[:,ilat,ipress]     # kelvin
-Q = r.solve(rtemp, p)
-rate_spec = np.array([0, 1, 2, 3])
-rc = 'navy orange limegreen r deepskyblue'.split()
-labels = r.species
-
-ilon = np.concatenate([[nphase-1], np.arange(nphase), [0]])
-phase = obs_phase[ilon]
-phase[0] -= 1
-phase[-1] += 1
-logQ = np.log(Q[:, ilon])
-
+q_equil = abundances[2][::4]
+molecs = 'H2O CO CO2 CH4'.split()
+nmol = len(molecs)
+cols = 'navy limegreen red orange'.split()
 
 
 fs = 11
@@ -99,6 +81,10 @@ bot, dy = 0.10, 0.88
 left1, left2, dx1, dx2 = 0.09, 0.41, 0.25, 0.56
 logxticks = 0.8, 1.0, 1.4, 2.0, 3.0, 4.0, 5.0
 sigma = 15.0
+
+iplot = np.arange(nphase+1) % nphase
+plot_phase = obs_phase[iplot]
+plot_phase[-1] += 1
 
 
 fig = plt.figure(47, (8.5, 4.5))
@@ -122,6 +108,7 @@ ax2.set_ylim(-2.5, 13.0)
 ax2.text(-0.02, -0.75, r"32x64 lat$-$lon grid", fontsize=fs-2)
 ax2.text(-0.09, -1.4, "16 longitudinal slices", fontsize=fs-2)
 
+# Temperatures:
 ax = fig.add_axes([0.068, 0.11, 0.15, 0.44])
 for i in range(nphase):
     plt.semilogy(temps[i,1], press, lw=1.5, c=bicolor(obs_phase[i]))
@@ -132,12 +119,13 @@ ax.set_ylabel('Pressure (bar)', fontsize=fs-1)
 plt.xlabel('Temperature (K)', fontsize=fs-1)
 ax.tick_params(labelsize=fs-2, direction='in', which='both')
 
+# Spectra:
 ax = fig.add_axes([0.29, bot, 0.63, dy])
 plt.xscale('log')
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax.set_xticks(logxticks)
-for iphase in range(nphase):
-    ax.plot(wl, gaussf(flux[iphase],sigma)*pc.ppt, c=bicolor(obs_phase[iphase]))
+for i in range(nphase):
+    ax.plot(wl, gaussf(flux[2,i],sigma)*pc.ppt, c=bicolor(obs_phase[i]))
 ax.set_xlim(0.8, 5.5)
 ax.set_ylim(0, 110)
 ax.set_xlabel('Wavelength (um)', fontsize=fs)
@@ -154,9 +142,12 @@ cb1.set_label('Orbital phase', fontsize=fs)
 ax1.tick_params(labelsize=fs-1, direction='in')
 cb1.update_ticks()
 
+# Abundances:
 ax = fig.add_axes([0.364, 0.69, 0.22, 0.28])
-for j,ir in enumerate(rate_spec):
-    ax.plot(phase, Q[ir, ilon], 'o-', c=rc[j], ms=2, label=r.species[ir])
+for j in range(nmol):
+    ax.plot(
+        plot_phase, q_equil[iplot,j], 'o-', color=cols[j], ms=2.0,
+        label=molecs[j])
 ax.tick_params(labelsize=fs-2, direction='in')
 ax.set_yscale('log')
 ax.set_xlabel("Orbital Phase", fontsize=fs-1)
@@ -165,82 +156,11 @@ ax.set_xticks([0, 0.5, 1.0])
 ax.set_yticks([1e-3, 1e-6, 1e-9])
 ax.set_xlim(0, 1)
 ax.set_ylim(3e-11, 3e-3)
-ax.legend(fontsize=fs-2.5, loc=(1.03,0.46),
+ax.legend(
+    fontsize=fs-2.5, loc=(1.03,0.46),
     labelspacing=0.2,
     handlelength=1.25,
     handletextpad=0.5,
     )
 plt.savefig('../plots/WASP43b_disk_integrated_model_emission.pdf')
 
-
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-fig = plt.figure(48, (8.5, 4.5))
-plt.clf()
-
-ax = plt.axes([-0.06, 0.55, 0.35, 0.55], projection='3d')
-ax.axis('off')
-ax.view_init(azim, elev)
-ax.plot_surface(
-    x, y, z, facecolors=colors, linewidth=0.0, antialiased=True, zorder=3)
-ax.plot_wireframe(x_cell, y_cell, z_cell, color='k', linewidth=1.0, zorder=5)
-ax.plot_wireframe(
-    x_slice, y_slice, z_slice, color='k', linewidth=1.0, zorder=5, ccount=0)
-
-ax2 = plt.axes([-0.03, 0.55, 0.35, 0.88])
-ax2.axis('off')
-ax2.plot(xx+0.01, yy, color='0.5')
-ax2.plot(-xx+0.087, 1.1*yy+0.6, color='0.5')
-ax2.set_xlim(-0.22, 0.5)
-ax2.set_ylim(-2.5, 13.0)
-ax2.text(-0.02, -0.75, r"32x64 lat$-$lon grid", fontsize=fs-2)
-ax2.text(-0.09, -1.4, "16 longitudinal slices", fontsize=fs-2)
-
-ax = fig.add_axes([0.068, 0.11, 0.16, 0.44])
-for i in range(nphase):
-    plt.semilogy(temps[i,1], press, lw=1.5, c=bicolor(obs_phase[i]))
-ax.set_yticks(np.logspace(2, -6, 5))
-ax.set_ylim(np.amax(press), 1e-7)
-ax.set_xlim(250, 2200)
-ax.set_ylabel('Pressure (bar)', fontsize=fs-1)
-plt.xlabel('Temperature (K)', fontsize=fs-1)
-ax.tick_params(labelsize=fs-2, direction='in', which='both')
-
-ax = fig.add_axes([0.29, bot, 0.63, dy])
-plt.xscale('log')
-ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax.set_xticks(logxticks)
-for iphase in range(nphase):
-    flux_ratio = flux[iphase]/starflux * rprs**2 / pc.ppt
-    plt.plot(wl, gaussf(flux_ratio,sigma), c=bicolor(obs_phase[iphase]))
-ax.set_xlim(0.8, 5.5)
-ax.set_ylim(0, 5.3)
-ax.set_xlabel('Wavelength (um)', fontsize=fs)
-ax.set_ylabel(r'Disk-integrated $F_{\rm p}/F_{\rm s}$ (ppt)', fontsize=fs)
-ax.tick_params(labelsize=fs-1, direction='in')
-
-ax1 = fig.add_axes([0.925, bot, 0.015, dy])
-norm = matplotlib.colors.Normalize(vmin=0, vmax=1.0)
-cb1 = matplotlib.colorbar.ColorbarBase(
-    ax1, cmap=bicolor, norm=norm, ticks=np.linspace(0,1,11),
-    orientation='vertical')
-cb1.set_label('Orbital phase', fontsize=fs)
-ax1.tick_params(labelsize=fs-1, direction='in')
-cb1.update_ticks()
-
-ax = fig.add_axes([0.37, 0.65, 0.25, 0.3])
-for j,ir in enumerate(rate_spec):
-    ax.plot(phase, Q[ir, ilon], 'o-', c=rc[j], ms=2, label=r.species[ir])
-ax.tick_params(labelsize=fs-2, direction='in')
-ax.set_yscale('log')
-ax.set_xlabel("Orbital Phase", fontsize=fs-1)
-ax.set_ylabel(r'Volume mixing ratio', fontsize=fs-1)
-ax.set_xticks([0, 0.5, 1.0])
-ax.set_yticks([1e-3, 1e-6, 1e-9])
-ax.set_xlim(0, 1)
-ax.set_ylim(3e-11, 3e-3)
-ax.legend(fontsize=fs-2.5, loc=(1.03,0.5),
-    labelspacing=0.2,
-    handlelength=1.25,
-    handletextpad=0.5,
-    )
-plt.savefig('../plots/WASP43b_disk_integrated_model_flux_ratio.pdf')
--- code/model_WASP43b.py
+++ code/model_WASP43b.py
@@ -2,21 +2,19 @@ import sys
 import warnings
 import time
 import os
-import pickle as pk
 
 import numpy as np
+
 import pyratbay as pb
 import pyratbay.atmosphere as pa
 import pyratbay.constants as pc
 import pyratbay.spectrum as ps
-import pyratbay.tools as pt
 import pyratbay.io as io
 import _trapz as t
 
 sys.path.append('../rate')
 import rate
 sys.path.append('../pandexo')
-import pandexo.engine.justplotit as jpi
 
 
 def get_area(lon, lat):
@@ -79,96 +77,137 @@ def project(lon, lat, phase):
 
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-# Now a 3D planet:
-
+# Load 3D TP model of WASP-43b:
+# Originated from WASP-34b GCM from Venot et al. (2020), but with
+# simplifiled TP profiles (Madhu TP fits to GCM)
 with np.load('WASP43b_3D_temperature_madhu_model.npz') as gcm_data:
     temps = gcm_data['temp']
     pressure = gcm_data['press']
-    lats  = gcm_data['lats']
-    lons  = gcm_data['lons']
-    phase = gcm_data['phase']
-    obs_phase = gcm_data['obs_phase']
     obs_ilat = gcm_data['obs_ilat']
+    obs_phase = gcm_data['obs_phase']
+
+    lats = gcm_data['lats']
+    lons = gcm_data['lons']
+    phase = gcm_data['phase']
+
 
 nlat = len(lats)
 nlon = len(lons)
+nlayers = len(pressure)
+nphase = len(obs_phase)
 
-
-# Initialize object with solar composition:
+# Chemistry in equilibrium for solar composition at 0.1 bar and lat 60 deg:
 r = rate.Rate(C=2.5e-4, N=1.0e-4, O=5.0e-4, fHe=0.0851)
 ipress = 42  # ~0.1 bar
-ilat = 0  # -60 deg  (Nice switch from CO--CH4 dominated atm)
+ilat_q = 0  # -60 deg  (Nice switch from CO--CH4 dominated atm)
 p = np.tile(pressure[ipress]/pc.bar, len(obs_phase))  # bars
-rtemp = temps[:, ilat, ipress]     # kelvin
-Q = r.solve(rtemp, p)
+rtemp = temps[:, ilat_q, ipress]
+Q_equil = r.solve(rtemp, p)
 labels = r.species
 
-
 pyrat = pb.run('model_WASP43b.cfg')
+nwave = pyrat.spec.nwave
 press = pyrat.atm.press / pc.bar
 wavelength = 1.0 / (pyrat.spec.wn*pc.um)
 rprs = pyrat.phy.rplanet/pyrat.phy.rstar
-
 q0 = np.copy(pyrat.atm.qbase)
 imol = [labels.index(molfree) for molfree in pyrat.atm.molfree]
-nwave = pyrat.spec.nwave
+nmol = len(pyrat.atm.molfree)
 
-nphase = len(obs_phase)
 mu = project(np.radians(lons), np.radians(lats), phase=obs_phase)
 area = get_area(np.radians(lons), np.radians(lats))
 pyrat.log.verb = 0
 
 
-ilatt = 10  # Use temperature profiles at -30 deg
-obs_lat = 1
-t0 = time.time()
-spectra = np.zeros((nphase,nwave))
+# Feng et al. (2020):      H2O    CO    CO2   CH4
+Q_constant = 10**np.array([-3.37, -3.7, -9.0, -9.0])
+
+# Discrete temperature slices:
+temps_02S = np.zeros((nlon,nlayers))  # 2 hemispheres (day/night)
+temps_09S = np.zeros((nlon,nlayers))  # 9 slices (symmetric day--night)
+temps_16S = np.zeros((nlon,nlayers))  # 16 slices (GCM asymmetric+offset)
+
+# Discrete abundance slices:
+Q_01S = np.zeros((nlon,nmol))  # Uniform
+Q_16S = np.zeros((nlon,nmol))  # 16 slices (equilibrium chemistry)
+
+# Temperature profiles at lat 30 deg:
+ilat_temp = 1
 for ilon in range(nlon):
-    vis_phase = mu[:,ilon,0] > 0
-    if not np.any(vis_phase):
-        continue
-    iphase = np.where(vis_phase)[0]
+    if np.abs(phase[ilon]-0.5) < 0.25:
+        temps_02S[ilon] = temps[ 7,ilat_temp]
+    else:
+        temps_02S[ilon] = temps[15,ilat_temp]
 
     ophase = (
         obs_phase
         - (phase[ilon]<0.5)*(obs_phase-phase[ilon]> 0.5)
         + (phase[ilon]>0.5)*(obs_phase-phase[ilon]<-0.5))
-    ilon_discrete = np.argmin(np.abs(phase[ilon] - ophase))
+    ilon_16 = np.argmin(np.abs(phase[ilon] - ophase))
+    ilon_09 = 7 + np.abs(ilon_16-8)
 
-    q2 = pa.qscale(
-        q0, pyrat.mol.name, pyrat.atm.molmodel,
-        pyrat.atm.molfree, np.log10(Q[imol,ilon_discrete]),
-        pyrat.atm.bulk, iscale=pyrat.atm.ifree, ibulk=pyrat.atm.ibulk,
-        bratio=pyrat.atm.bulkratio, invsrat=pyrat.atm.invsrat)
-    # Discrete TP per slice:
-    temp = temps[ilon_discrete, obs_lat]
-    status = pb._ra.update_atm(pyrat, temp, q2, None)
-    pb._cs.interpolate(pyrat)
-    pb._ray.absorption(pyrat)
-    pb._al.absorption(pyrat)
-    pb._od.optical_depth(pyrat)
-    ps.blackbody_wn_2D(
-        pyrat.spec.wn, pyrat.atm.temp, pyrat.od.B, pyrat.od.ideep)
-    for ilat in range(nlat//2):
-        intensity = t.intensity(
-            pyrat.od.depth, pyrat.od.ideep, pyrat.od.B,
-            mu[iphase,ilon,ilat], pyrat.atm.rtop)
-        spectra[iphase] += 2*intensity * mu[iphase,ilon,ilat,np.newaxis] \
-                        * area[ilon,ilat]
-    print(f'Longitude [{ilon:2d}]: {lons[ilon]:.1f} deg')
-t1 = time.time()
-print(t1-t0)
+    temps_09S[ilon] = temps[ilon_09,ilat_temp]
+    temps_16S[ilon] = temps[ilon_16,ilat_temp]
+
+    Q_01S[ilon] = Q_constant
+    Q_16S[ilon] = Q_equil[imol, ilon_16]
+
+
+models = [
+    {'abund': Q_01S, 'temp': temps_02S},
+    {'abund': Q_01S, 'temp': temps_09S},
+    {'abund': Q_16S, 'temp': temps_16S},
+]
+nmodels = len(models)
+
+spectra = np.zeros((nmodels,nphase,nwave))
+for i, model in enumerate(models):
+    t0 = time.time()
+    abunds = model['abund']
+    temps  = model['temp']
+    for ilon in range(nlon):
+        vis_phase = mu[:,ilon,0] > 0
+        if not np.any(vis_phase):
+            continue
+        iphase = np.where(vis_phase)[0]
+
+        q2 = pa.qscale(
+            q0, pyrat.mol.name, pyrat.atm.molmodel,
+            pyrat.atm.molfree, np.log10(abunds[ilon]),
+            pyrat.atm.bulk, iscale=pyrat.atm.ifree, ibulk=pyrat.atm.ibulk,
+            bratio=pyrat.atm.bulkratio, invsrat=pyrat.atm.invsrat)
+        status = pb._ra.update_atm(pyrat, temps[ilon], q2, None)
+        pb._cs.interpolate(pyrat)
+        pb._ray.absorption(pyrat)
+        pb._al.absorption(pyrat)
+        pb._od.optical_depth(pyrat)
+        ps.blackbody_wn_2D(
+            pyrat.spec.wn, pyrat.atm.temp, pyrat.od.B, pyrat.od.ideep)
+        for ilat in range(nlat//2):
+            intensity = t.intensity(
+                pyrat.od.depth, pyrat.od.ideep, pyrat.od.B,
+                mu[iphase,ilon,ilat], pyrat.atm.rtop)
+            spectra[i, iphase] += \
+                2*intensity * mu[iphase,ilon,ilat,np.newaxis] * area[ilon,ilat]
+        print(f'Longitude [{ilon:2d}]: {lons[ilon]:6.1f} deg')
+    t1 = time.time()
+    print(t1-t0)
 
 # Save disk-integrated model emission spectra to file:
-np.savez('WASP43b_3D_synthetic_emission_spectra.npz',
+np.savez(
+    'WASP43b_3D_synthetic_emission_spectra.npz',
     wavelength=wavelength,
     spectra=spectra,
     starflux=pyrat.spec.starflux,
     rprs=rprs,
-    obs_phase=obs_phase)
+    obs_phase=obs_phase,
+    abundances=np.array([Q_01S, Q_01S, Q_16S]),
+    temperatures=np.array([temps_02S, temps_09S, temps_16S]),
+    molecules=pyrat.atm.molfree,
+    phase=phase,
+)
 
 
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # Pandexo these monsters:
 
@@ -178,14 +217,22 @@ with np.load('WASP43b_3D_synthetic_emission_spectra.npz') as emission_model:
     starflux = emission_model['starflux']
     obs_phase = emission_model['obs_phase']
     rprs = float(emission_model['rprs'])
+    abundances = emission_model['abundances']
+    temperatures = emission_model['temperatures']
 
 pyrat = pb.run('model_WASP43b.cfg')
-nphase = len(obs_phase)
+nmodels, nphase, nwave = np.shape(spectra)
+
+models = [
+    {'abund': q, 'temp': t}
+    for q,t in zip(abundances, temperatures)
+]
+
 
 # Show JWST instruments:
 # print(jdi.print_instruments())
 
-# Hellier (2011)
+# Hellier (2011):
 period = 0.813475 * pc.day
 tdur = 1.1592 * pc.hour
 
@@ -206,107 +253,95 @@ instruments = [
 # SOSS gives lower noise than PRISM
 # PRISM reaches 0.6 um, but there's too little flux already.
 # G395H is enough to complement SOSS
-# So, I'll stitch to those two
+# So, I'll stitch those two
+
+
+filters = sorted([
+    f'../inputs/filters/{filt}'
+    for filt in os.listdir('../inputs/filters')
+    if filt.startswith('JWST')])
+filter_wn, filter_trans = [], []
+for filt in filters:
+    wn, trans = io.read_spectrum(filt)
+    filter_wn.append(wn)
+    filter_trans.append(trans)
+
+nwave_obs = len(filters)
+flux_ratio = spectra / starflux * rprs**2
+
+noiseless_flux_ratio = np.zeros((nmodels, nphase, nwave_obs))
+for i in range(nmodels):
+    for j in range(nphase):
+        noiseless_flux_ratio[i,j] = ps.band_integrate(
+            flux_ratio[i,j], 1e4/wl, filter_trans, filter_wn)
+
 
 # Note, pandexo's resolution is R = 0.5 * lambda/dlambda
 resolution = 100.0
 noise = [20.0, 30.0]
-
+n_transits = 1
 pyrat.ncpu = 5  # 24 was breaking pandexo/pandeia
-pandexo_wl = []
-pandexo_flux = []
-pandexo_uncert = []
-for iphase in range(nphase):
-    print(f'\nThis is phase {iphase+1}/{nphase}:')
-    pyrat.spec.spectrum = spectra[iphase]
-    pflux, puncert, pwl = [], [], []
-    for instrument, noise_floor in zip(instruments, noise):
-        save_file = f'new_pandexo_WASP43b_phase{obs_phase[iphase]:.2f}_{"-".join(instrument.split())}.p'
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            pandexo_sim = io.export_pandexo(
-                pyrat, baseline, transit_duration,
-                Kmag=Kmag, metal=metal, instrument=instrument, n_transits=1,
-                resolution=resolution, noise_floor=noise_floor,
-                save_file=save_file)
-        pwl.append(pandexo_sim[1][0])
-        pflux.append(pandexo_sim[2][0])
-        puncert.append(pandexo_sim[3][0])
-    pandexo_wl    .append(pwl)
-    pandexo_flux  .append(pflux)
-    pandexo_uncert.append(puncert)
 
-# All phases have same wavelength, so, keep only the first one:
-pandexo_wl = pandexo_wl[0]
-
-pwl = np.concatenate(pandexo_wl)
-pflux   = np.array([np.concatenate(pflux) for pflux in pandexo_flux])
-puncert = np.array([np.concatenate(punc) for punc in pandexo_uncert])
-
-
-filters = sorted([
-    f'../inputs/filters/{filt}' for filt in os.listdir('../inputs/filters')
-    if filt.startswith('JWST')])
-fwn, ftrans = [], []
-for filt in filters:
-    wn, trans = io.read_spectrum(filt)
-    fwn.append(wn)
-    ftrans.append(trans)
-
-flux_ratio = flux/starflux * rprs**2
-
-noiseless_flux_ratio = np.array([
-    pt.band_integrate(flux_rat, 1e4/wl, ftrans, fwn)
-    for flux_rat in flux_ratio])
+pflux = np.zeros((nmodels, nphase, nwave_obs))
+puncert = np.zeros((nmodels, nphase, nwave_obs))
+for i in range(nmodels):
+    for iphase in range(nphase):
+        if iphase > 8 and i != 2:
+            pflux[i,iphase] = pflux[i,nphase-iphase]
+            puncert[i,iphase] = puncert[i,nphase-iphase]
+            continue
+        print(f'\nThis is phase {iphase+1}/{nphase} of model {i}:')
+        pyrat.spec.spectrum = spectra[i,iphase]
+        pandexo_wl = []
+        pandexo_flux = []
+        pandexo_uncert = []
+        for instrument, noise_floor in zip(instruments, noise):
+            save_file = (
+                f'pandexo_WASP43b_model{i:02d}_'
+                f'phase{obs_phase[iphase]:.2f}_'
+                f'{"-".join(instrument.split())}.p')
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                pandexo_sim = io.export_pandexo(
+                    pyrat, baseline, transit_duration,
+                    Kmag=Kmag, metal=metal, instrument=instrument,
+                    n_transits=n_transits, resolution=resolution,
+                    noise_floor=noise_floor, save_file=save_file)
+            pandexo_wl.append(pandexo_sim[1][0])
+            pandexo_flux.append(pandexo_sim[2][0])
+            pandexo_uncert.append(pandexo_sim[3][0])
+        pflux[i,iphase] = np.concatenate(pandexo_flux)
+        puncert[i,iphase] = np.concatenate(pandexo_uncert)
 
 
-#np.savez(f'WASP43b_3D_synthetic_pandexo_flux_ratios.npz',
-#    flux_ratio=flux_ratio,
-#    wavelength=wl,
-#    stellar_flux=pyrat.spec.starflux,
-#    pandexo_flux_ratio=pflux,
-#    noiseless_flux_ratio=noiseless_flux_ratio,
-#    pandexo_uncert=puncert,
-#    pandexo_wl=pwl,
-#    phase=obs_phase,
-#    flux_units='erg s-1 cm-2 cm',
-#    wl_units='micron')
+# Model evaluated at properties of local (sub-observer) longitude
+# for each orbital phase:
+local_flux_ratio = np.zeros((nmodels, nphase, nwave_obs))
+for i in range(nmodels):
+    abunds = models[i]['abund']
+    temps  = models[i]['temp']
+    for iphase in range(nphase):
+        q2 = pa.qscale(
+            pyrat.atm.qbase, pyrat.mol.name, pyrat.atm.molmodel,
+            pyrat.atm.molfree, np.log10(abunds[4*iphase]),
+            pyrat.atm.bulk, iscale=pyrat.atm.ifree, ibulk=pyrat.atm.ibulk,
+            bratio=pyrat.atm.bulkratio, invsrat=pyrat.atm.invsrat)
+        pyrat.run(temps[4*iphase], q2)
+        local_fratio = pyrat.spec.spectrum / pyrat.spec.starflux * rprs**2
+        local_flux_ratio[i,iphase] = ps.band_integrate(
+            local_fratio, 1e4/wl, filter_trans, filter_wn)
 
 
-pfiles = sorted([
-    pfile for pfile in os.listdir()
-    if pfile.endswith('.p')])
-
-pandexo_flux = []
-pandexo_uncert = []
-for i in range(nphase):
-    print(i)
-    pwl = []
-    pflux = []
-    puncert = []
-    for j in range(2):
-        with open(pfiles[2*i+j], 'br') as pick:
-            out = pk.load(pick)
-        x, y, err = jpi.jwst_1d_spec(
-            out, R=resolution, num_tran=1, model=False, plot=False)
-        pwl += list(x[0])
-        pflux += list(y[0])
-        puncert += list(err[0])
-    pandexo_flux.append(pflux)
-    pandexo_uncert.append(puncert)
-pandexo_wl = np.array(pwl)
-pandexo_flux = np.array(pandexo_flux)
-pandexo_uncert = np.array(pandexo_uncert)
-
-
-np.savez('WASP43b_3D_synthetic_pandexo_flux_ratios.npz',
+np.savez(
+    'WASP43b_3D_synthetic_pandexo_flux_ratios.npz',
     flux_ratio=flux_ratio,
     wavelength=wl,
-    stellar_flux=starflux,
+    stellar_flux=pyrat.spec.starflux,
+    pandexo_flux_ratio=pflux,
+    pandexo_uncert=puncert,
+    pandexo_wl=np.concatenate(pandexo_wl),
     noiseless_flux_ratio=noiseless_flux_ratio,
-    pandexo_flux_ratio=pandexo_flux,
-    pandexo_uncert=pandexo_uncert,
-    pandexo_wl=pandexo_wl,
+    local_flux_ratio=local_flux_ratio,
     phase=obs_phase,
     flux_units='erg s-1 cm-2 cm',
     wl_units='micron')
